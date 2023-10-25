55cf3d44d47ff3eb58f407fd89ce9dbf8a8a739d
Khuong Nguyen-Ngo <khuongnguyenngo@gmail.com>
2023-10-24 21:07:32 -0400

    Created test file for NFA.java

---
+++ test/NFATest.java
@@ -0,0 +1,8 @@
+import nfa_to_dfa.NFA;
+
+public class NFATest {
+	public static void main(String[] args) {
+		// test creating a graph, manually
+		NFA d = new NFA("C:\\Users\\depre\\Documents\\KDN_Workspace\\nfa-to-dfa\\test\\readable\\test.txt");
+	}
+}
