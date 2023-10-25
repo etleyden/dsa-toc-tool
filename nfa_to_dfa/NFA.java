bfc4ec69653b7c138e0123fd96de88fc73f895c9
Khuong Nguyen-Ngo <khuongnguyenngo@gmail.com>
2023-10-25 14:32:50 -0400

    Redudant

--- nfa_to_dfa/NFA.java
+++ nfa_to_dfa/NFA.java
@@ -1,4 +1,5 @@
 package nfa_to_dfa;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -11,13 +12,20 @@ public class NFA extends Graph {
             File text_file = new File(path);
             Scanner reader = new Scanner(text_file);
 
-            while(reader.hasNextLine()) {
+            while (reader.hasNextLine()) {
                 String data = reader.nextLine();
                 System.out.println(data);
             }
         } catch (FileNotFoundException e) {
             System.out.println("NOT FOUND");
             e.printStackTrace();
+        }
+    }
+
+    public void addStates(String input) {
+        String[] nodes = input.split(" ");
+        for (String curr : nodes) {
+            this.addNode(curr);
         }
     }
 
--- test/NFATest.java
+++ test/NFATest.java
@@ -4,5 +4,6 @@ public class NFATest {
 	public static void main(String[] args) {
 		// test creating an NFA, manually
 		NFA d = new NFA("C:\\Users\\depre\\Documents\\KDN_Workspace\\nfa-to-dfa\\test\\readable\\test.txt");
+
 	}
 }
