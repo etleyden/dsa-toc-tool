3949f81c9dadae0890c7ccd8a45fd8286cb69937
Khuong Nguyen-Ngo <khuongnguyenngo@gmail.com>
2023-11-22 23:32:35 -0500

    Added NFA test cases, finished parseFile method, finished hasTransition method, added test libraries and JSON library for VSCode

--- src/dsa_toc_tool/NFA.java
+++ src/dsa_toc_tool/NFA.java
@@ -1,17 +1,30 @@
 package dsa_toc_tool;
 
 import java.io.FileReader;
+<<<<<<< HEAD
+=======
+import java.util.ArrayList;
+import java.lang.reflect.Array;
+>>>>>>> e2aa24e (Added NFA test cases, finished parseFile method, finished hasTransition method, added test libraries and JSON library for VSCode)
 import org.json.simple.JSONObject;
 import org.json.simple.JSONArray;
 import org.json.simple.parser.*;
 
+<<<<<<< HEAD
 public class NFA {
     private Graph graph;
+=======
+public class NFA extends Graph {
+>>>>>>> e2aa24e (Added NFA test cases, finished parseFile method, finished hasTransition method, added test libraries and JSON library for VSCode)
     private int num_states;
     private int start_num;
     private JSONArray input_alphabet;
     private int start_state;
     private JSONArray accept_states;
+<<<<<<< HEAD
+=======
+    private JSONArray transitions;
+>>>>>>> e2aa24e (Added NFA test cases, finished parseFile method, finished hasTransition method, added test libraries and JSON library for VSCode)
 
     /**
      * Takes the filepath to a .json description of the NFA, and generates an
@@ -20,6 +33,7 @@ public class NFA {
      * @param filepath the relative path to the json file.
      */
     public NFA(String filepath) {
+<<<<<<< HEAD
         this();
         parseFile(filepath);
     }
@@ -42,6 +56,20 @@ public class NFA {
      * Adds edges to nodes of NFA's graph object
      * 
      * @param filepath path of JSON file
+=======
+        super(); // added temporarily to test out build settings
+        parseFile(filepath);
+    }
+
+    public NFA() {
+
+    }
+
+    /**
+     * Parse JSON file into NFA
+     * 
+     * @param filePath path of JSON file
+>>>>>>> e2aa24e (Added NFA test cases, finished parseFile method, finished hasTransition method, added test libraries and JSON library for VSCode)
      */
     public void parseFile(String filepath) {
         try {
@@ -51,12 +79,16 @@ public class NFA {
             if (!(dsa_type.equals("nfa"))) {
                 throw new Exception();
             }
+<<<<<<< HEAD
             // Parse JSON into NFA variables
+=======
+>>>>>>> e2aa24e (Added NFA test cases, finished parseFile method, finished hasTransition method, added test libraries and JSON library for VSCode)
             this.num_states = (int) (long) o.get("num_states");
             this.start_num = (int) (long) o.get("start_numbering");
             this.input_alphabet = (JSONArray) o.get("input_alphabet");
             this.start_state = (int) (long) o.get("start_state");
             this.accept_states = (JSONArray) o.get("accept_states");
+<<<<<<< HEAD
             JSONArray transitions = (JSONArray) o.get("transitions");
             for (int i = start_num; i <= num_states; i++) {
                 // Adds nodes to graph
@@ -75,6 +107,24 @@ public class NFA {
                         graph.setEdge(curr_state, next_state, Integer.parseInt(input));
                     } else {
                         graph.setEdge(curr_state, next_state, input);
+=======
+            this.transitions = (JSONArray) o.get("transitions");
+            for (int i = start_num; i <= num_states; i++) {
+                this.addNode(i);
+                System.out.println("Node " + i + " " + nodeExists(i));
+            }
+            for (int i = 0; i < transitions.size(); i++) {
+                JSONArray transition = (JSONArray) transitions.get(i);
+                int curr_state = i + 1;
+                for (int j = 0; j < transition.size(); j++) {
+                    JSONArray pair = (JSONArray) transition.get(j);
+                    String input = (String) pair.get(0);
+                    int next_state = (int) (long) pair.get(1);
+                    if ((input.equals(""))) {
+                        setEdge(curr_state, next_state, input);
+                    } else {
+                        setEdge(curr_state, next_state, Integer.parseInt(input));
+>>>>>>> e2aa24e (Added NFA test cases, finished parseFile method, finished hasTransition method, added test libraries and JSON library for VSCode)
                     }
                 }
             }
@@ -83,6 +133,10 @@ public class NFA {
             e.printStackTrace();
         }
     }
+<<<<<<< HEAD
+=======
+
+>>>>>>> e2aa24e (Added NFA test cases, finished parseFile method, finished hasTransition method, added test libraries and JSON library for VSCode)
     /**
      * Verifies a transition between two states and a given input
      * 
@@ -92,6 +146,7 @@ public class NFA {
      * @return true if the transition exists, false if not
      */
     public boolean hasTransition(int startState, String input, int endState) {
+<<<<<<< HEAD
         try {
             GraphEdge n = graph.getEdge(startState, endState);
             GraphEdge.Type type = n.getType();
@@ -104,6 +159,21 @@ public class NFA {
             }
         } catch (NullPointerException e) {
             return false;
+=======
+        for (int i = 0; i < transitions.size(); i++) {
+            int curr_state = i + 1;
+            if (curr_state == startState) {
+                JSONArray transition = (JSONArray) transitions.get(i);
+                for (int j = 0; j < transition.size(); j++) {
+                    JSONArray pair = (JSONArray) transition.get(j);
+                    String inp = (String) pair.get(0);
+                    int next_state = (int) (long) pair.get(1);
+                    if ((inp.equals(input)) && next_state == endState) {
+                        return true;
+                    }
+                }
+            }
+>>>>>>> e2aa24e (Added NFA test cases, finished parseFile method, finished hasTransition method, added test libraries and JSON library for VSCode)
         }
         return false;
     }
--- tests/dsa_toc_tool/NFATest.java
+++ tests/dsa_toc_tool/NFATest.java
@@ -1,4 +1,5 @@
 import dsa_toc_tool.NFA;
+<<<<<<< HEAD
 
 import dsa_toc_tool.NFA;
 import org.junit.jupiter.api.Test;
@@ -11,12 +12,29 @@ class NFATest {
         NFA nfa = new NFA("example_json/nfa1.json");
         assertTrue(nfa.hasTransition(1, "1", 1));
         assertTrue(nfa.hasTransition(1, "0", 1));
+=======
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+public class NFATest {
+    public static void main(String[] args) {
+        NFA test = new NFA("example_json\\nfa1.json");
+    }
+
+    @Test
+    public void test_hasTransition() {
+        NFA nfa = new NFA("example_json/nfa1.json");
+        assertTrue(nfa.hasTransition(1, "1", 1));
+        assertTrue(nfa.hasTransition(1, "1", 2));
+>>>>>>> e2aa24e (Added NFA test cases, finished parseFile method, finished hasTransition method, added test libraries and JSON library for VSCode)
         assertTrue(nfa.hasTransition(2, "", 3));
         assertFalse(nfa.hasTransition(0, "", 4));
         assertFalse(nfa.hasTransition(4, "", 4));
         assertFalse(nfa.hasTransition(2, "1", 3));
     }
 
+<<<<<<< HEAD
     @Test
     void doesAccept() {
         NFA nfa = new NFA("example_json/nfa1.json");
@@ -35,4 +53,17 @@ class NFATest {
     @Test
     void convertToRegularExpression() {
     }
+=======
+    /*
+     * public void doesAccept() {
+     * NFA nfa = new NFA("example_json/nfa1.json");
+     * assertTrue(nfa.doesAccept("1111010101"));
+     * assertFalse(nfa.doesAccept(""));
+     * assertTrue(nfa.doesAccept("11"));
+     * assertFalse(nfa.doesAccept("1"));
+     * assertFalse(nfa.doesAccept("111000"));
+     * assertTrue(nfa.doesAccept("111010"));
+     * }
+     */
+>>>>>>> e2aa24e (Added NFA test cases, finished parseFile method, finished hasTransition method, added test libraries and JSON library for VSCode)
 }
